f5c1ec7aaf09ef05437bf992ae26a650beb1bd62
CTAPbIuMABP <CTAPbIuMABP@users.noreply.github.com>
2014-07-21 22:09:47 +0300

    Update clean_target.js
    enclosure `i` in a local scope

--- CyberCat/src/main/resources/javascript/clean_target.js
+++ CyberCat/src/main/resources/javascript/clean_target.js
@@ -1,3 +1,5 @@
-for(i=0;i<document.links.length;i++){
-  document.links[i].setAttribute("target", "");
-}
+(function(){
+  for(var i=0;i<document.links.length;i++){
+    document.links[i].setAttribute("target", "");
+  }
+})();
