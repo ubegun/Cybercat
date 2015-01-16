7c33594d1c1c172078d0fdab4d5881e61075a48d
Serhiy Savruk <savruksergiy@gmail.com>
2015-01-16 14:26:18 +0200

    Added platform support to abstraction(like getVersion()).

--- CyberCat/src/main/java/org/cybercat/automation/test/AbstractFeature.java
+++ CyberCat/src/main/java/org/cybercat/automation/test/AbstractFeature.java
@@ -1,11 +1,11 @@
 /**Copyright 2013 The Cybercat project
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *     
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,7 +19,7 @@ import org.cybercat.automation.core.Platform;
 
 /**
  * @author Ubegun
- * 
+ *
  */
 public abstract class AbstractFeature implements IVersionControl, IFeature{
 
@@ -28,15 +28,19 @@ public abstract class AbstractFeature implements IVersionControl, IFeature{
     /**
      * This is the only constructor available. It will be called to create your feature.
      */
-    protected AbstractFeature() {        
+    protected AbstractFeature() {
         super();
         platforms = getPlatforms();
     }
-    
-    /**
-     * Identifies version by default. can be redefined to change version feature.  
-     */
+
     @Override
+    public Platform[] getPlatforms() {
+        return new Platform[]{Platform.WEB, Platform.TABLET, Platform.MOBILE};
+    }
+
+    /**
+     * Identifies version by default. can be redefined to change version feature.
+     */
     @Override
     public int getVersion() {
         return 0;
@@ -51,5 +55,5 @@ public abstract class AbstractFeature implements IVersionControl, IFeature{
         }
         return false;
     }
-  
+
 }
